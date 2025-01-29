7bae863b1e364322dda1c67a3140191ad38c34d8
José Antonio das Neves Neto <ef128@uol.com.br>
2025-01-29 14:56:56 -0300

    edição do UserDTO - simplificação

--- CarteiraDigital/src/DigitalWalletAPI/DTOs/UserDTO.cs
+++ CarteiraDigital/src/DigitalWalletAPI/DTOs/UserDTO.cs
@@ -3,5 +3,5 @@ public class UserDTO
     public int Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public decimal Balance { get; set; }
+    public decimal Balance { get; set; } 
 }
